d1a9abd37d9791b3da3589a4d3d50273eb4e6722
Faizal <zafri_faizal@outlook.com>
2019-02-01 20:20:07 +0530

    adding many to many relationship

--- src/main/java/com/faizal/springrecipe/domain/Ingredient.java
+++ src/main/java/com/faizal/springrecipe/domain/Ingredient.java
@@ -22,7 +22,7 @@ public class Ingredient {
 	@OneToOne(fetch = FetchType.EAGER)
 	private UnitOfMeasure uom;
 
-	@ManyToOne()
+	@ManyToOne
 	private Recipe recipe;
 
 	public Long getId() {
--- src/main/java/com/faizal/springrecipe/domain/Notes.java
+++ src/main/java/com/faizal/springrecipe/domain/Notes.java
@@ -1,11 +1,13 @@
 package com.faizal.springrecipe.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
+@Entity
 public class Notes {
 
 	@Id
