fb9574d4662deff75840e2fd472b09d33d2e7eac
Faizal <zafri_faizal@outlook.com>
2019-02-01 22:59:51 +0530

    adding data jpa query methods

--- src/main/java/com/faizal/springrecipe/controllers/IndexController.java
+++ src/main/java/com/faizal/springrecipe/controllers/IndexController.java
@@ -1,13 +1,37 @@
 package com.faizal.springrecipe.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.faizal.springrecipe.domain.Category;
+import com.faizal.springrecipe.domain.UnitOfMeasure;
+import com.faizal.springrecipe.repositories.CategoryRepository;
+import com.faizal.springrecipe.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({ "", "/", "/index", "/index.html" })
 	public String getIndexPage() {
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+		System.out.println("cat : "+categoryOptional.get().getId());
+		System.out.println("uom : "+uomOptional.get().getId());
+		
 		return "index";
 	}
 }
--- src/main/java/com/faizal/springrecipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/faizal/springrecipe/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package com.faizal.springrecipe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.faizal.springrecipe.domain.Category;
 import com.faizal.springrecipe.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
+	Optional<UnitOfMeasure> findByDescription(String unitOfMeasure);
 }
