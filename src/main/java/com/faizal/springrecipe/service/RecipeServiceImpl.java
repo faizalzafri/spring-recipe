307989ff2d416f3ea6c0b29619f32629ba53d68e
Faizal <zafri_faizal@outlook.com>
2019-02-02 18:05:14 +0530

    adding lombok, refractored and removed stackoverflow error

--- src/main/java/com/faizal/springrecipe/service/RecipeServiceImpl.java
+++ src/main/java/com/faizal/springrecipe/service/RecipeServiceImpl.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 import com.faizal.springrecipe.domain.Recipe;
 import com.faizal.springrecipe.repositories.RecipeRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -19,6 +22,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+    	log.info("Executing getRecipes");
+    	
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
