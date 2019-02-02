9a3d6287dd9175e136d79de666358c169be584cb
Faizal <zafri_faizal@outlook.com>
2019-02-02 23:03:06 +0530

    Adding RecipeService Mock Test

---
+++ src/test/java/com/faizal/springrecipe/service/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.faizal.springrecipe.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.faizal.springrecipe.domain.Recipe;
+import com.faizal.springrecipe.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+	RecipeServiceImpl recipeServiceImpl;
+
+	@Mock
+	RecipeRepository recipeRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		recipeServiceImpl = new RecipeServiceImpl(recipeRepository);
+	}
+
+	@Test
+	public void getRecipes() throws Exception {
+
+		Recipe recipe = new Recipe();
+		HashSet<Recipe> hashSet = new HashSet<>();
+		hashSet.add(recipe);
+
+		when(recipeServiceImpl.getRecipes()).thenReturn(hashSet);
+
+		Set<Recipe> recipes = recipeServiceImpl.getRecipes();
+		assertEquals(recipes.size(), 1);
+		verify(recipeRepository, times(2)).findAll();
+	}
+
+}
