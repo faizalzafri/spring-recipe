b2163894830343aa1792c6b740f9cf197fc844dc
Faizal <zafri_faizal@outlook.com>
2019-02-06 22:17:26 +0530

    render image from db

--- src/main/java/com/faizal/springrecipe/controllers/ImageController.java
+++ src/main/java/com/faizal/springrecipe/controllers/ImageController.java
@@ -1,5 +1,12 @@
 package com.faizal.springrecipe.controllers;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.faizal.springrecipe.commands.RecipeCommand;
 import com.faizal.springrecipe.services.ImageService;
 import com.faizal.springrecipe.services.RecipeService;
 
@@ -36,5 +44,24 @@ public class ImageController {
 	public String showUploadForm(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.getRecipeCommandById(new Long(id)));
 		return "recipe/imageuploadform";
+	}
+
+	@GetMapping("/recipe/{id}/recipeimage")
+	public void renderImageFromDb(@PathVariable String id, HttpServletResponse response) throws IOException {
+
+		RecipeCommand reCommand = recipeService.getRecipeCommandById(new Long(id));
+
+		if (reCommand.getImage() != null) {
+			byte[] byteArray = new byte[reCommand.getImage().length];
+			int i = 0;
+
+			for (Byte wrappedByte : reCommand.getImage()) {
+				byteArray[i++] = wrappedByte; // auto unboxing
+			}
+
+			response.setContentType("image/jpeg");
+			InputStream is = new ByteArrayInputStream(byteArray);
+			IOUtils.copy(is, response.getOutputStream());
+		}
 	}
 }
